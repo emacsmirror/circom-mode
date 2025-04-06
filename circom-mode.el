6473decb0d89b756fcfc643ea302d476b9147186
Ta Quang Trung <taquangtrungvn@gmail.com>
2025-04-07 02:16:14 +0800

    refactor: optimize code, cf. #melpa/pull/9433 comment
    https://github.com/melpa/melpa/pull/9433#issuecomment-2781523682

--- circom-mode.el
+++ circom-mode.el
@@ -93,7 +93,7 @@
   (let ((pos (- (point-max) (point)))
         (indent (or indent (circom--calculate-indentation)))
         (shift-amount nil)
-        (beg (progn (beginning-of-line) (point))))
+        (beg (line-beginning-position)))
     (skip-chars-forward " \t")
     (if (null indent)
         (goto-char (- (point-max) pos))
